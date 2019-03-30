090d74f697436bb926543a3a4c7f9af9b37c1f76
Brian Kennedy <bpkennedy@gmail.com>
2019-03-29 19:26:28 -0500

    Cache local database and fix Travis test run

--- api/test/startup.js
+++ api/test/startup.js
@@ -1,11 +1,13 @@
 import { assert } from 'chai'
 import axios from 'axios'
 import * as server from '../src/server.js'
+const PORT = process.env.NODE_ENV === 'production' ? 3000 : 4000
+const baseUrl = `http://localhost:${PORT}`
 
 describe('Server startup', function () {
   
   beforeEach(async function () {
-    await server.start(true, 4000)
+    await server.start(true, PORT)
   })
   
   afterEach(async function () {
@@ -13,13 +15,13 @@ describe('Server startup', function () {
   })
   
   it('should find 1 ship from the initial migration', async function () {
-    const response = await axios.get('http://localhost:4000/api/1.0/ships')
+    const response = await axios.get(baseUrl + '/api/1.0/ships')
     assert.deepEqual(response.status, 200)
     assert.deepEqual(response.data[0].name, 'Z-95 Headhunter')
   })
   
   it('should validate server can run - no value, delete later', async function () {
-    const response = await axios.get('http://localhost:4000/documentation')
+    const response = await axios.get(baseUrl + '/documentation')
     assert.deepEqual(response.status, 200)
   })
   
