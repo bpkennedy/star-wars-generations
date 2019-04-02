3a05fd9c6ddec071ab1e8a9559a4232addecc47b
Brian Kennedy <bpkennedy@gmail.com>
2019-04-02 00:46:14 -0500

    Add theme colors, default fonts, and theme overrides

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div>
     <b-navbar type="dark" variant="dark">
       <b-navbar-nav>
-        <b-nav-item to="/">Star Wars Generations</b-nav-item>
+        <b-nav-item class="swg-logo" to="/">Star Wars Generations</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-item to="/login" right>Login</b-nav-item>
@@ -18,5 +18,11 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+  .swg-logo {
+    font-family: "Star Jedi", 'Roboto', Arial;
+    letter-spacing: .1rem;
+    a {
+      color: #f6f1b1;
+    }
+  }
 </style>
